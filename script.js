694858432a3091d10332b1e1b7823ba68e64fe98
Kashish Dudeja <121998675+kd1120@users.noreply.github.com>
2023-08-17 22:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+#btn{
+	color:white;
+	background-color:black;
+	border:none;
+	bordr-radius:5px;
+	button-type:submit;
+	
+	
+}
+#btn:hover{
+	color: yellow;
+	background-color:black;
+	border-radius:30px;
+}
